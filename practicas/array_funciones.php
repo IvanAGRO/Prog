90c957fc5ec1379ff261f0f1b9617be9d4e5e6ce
IvanAGRO <10203@cifpceuta.es>
2024-03-10 17:53:08 +0100

    Añadido practica funciones array

---
+++ practicas/array_funciones.php
@@ -0,0 +1,45 @@
+<?php
+define ("N",5);
+$vec=array(N);
+
+function generar (&$vec) {
+
+}
+
+function ver($vec) {
+
+}
+
+function suma($vec,&$sum) {
+
+}
+
+function mayor($vec,&$may) {
+
+}
+
+function menor($vec, &$menor) {
+
+}
+
+function busqueda_binaria($vec,$num) : boolean {
+
+
+
+    if ($encontrado) {
+        return TRUE;
+    }else {
+        return FALSE;
+    }
+}
+
+function ord_burbuja(&$vec) {
+
+}
+
+function rotar_derc(&$vec) {
+
+
+}
+
+//las llamadas no debeis tener problema
